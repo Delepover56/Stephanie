fe6686bfff05f437804814247bdbad68dfcf9f1c
delepover56 <taha82426980@gmail.com>
2024-08-03 00:47:01 +0500

    8th update

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,10 @@ module.exports = {
         "custom-peach": "22px -21px 0px -2px #FBF2EE",
       },
       screens: {
+        "max-2560": { max: "2560px" },
+        "max-1922": { max: "1922px" },
+        "max-1600": { max: "1600px" },
+        "max-1440": { max: "1440px" },
         "max-1200": { max: "1200px" },
         "max-992": { max: "992px" },
         "max-768": { max: "768px" },
